684a6966bc2a2322a2720c90c0c25d85fe441584
Treasurechic <babychic140@gmail.com>
2022-10-13 20:25:34 +0100

    :bento: header image upload

--- src/home/header.tsx
+++ src/home/header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import inspo from "assets/Images/Inspo.svg";
 import { Icon } from "@iconify/react";
 import { Navbar } from "./navbar";
@@ -6,14 +6,14 @@ import { Navbar } from "./navbar";
 export const Header = ({ scrollPos }: { scrollPos: number }) => {
   const aboutSection = document.querySelector("#about");
 
-  // useEffect(() => {
-  //   if (scrollPos === 0) {
-  //     document.body.classList.add("no-scroll");
-  //     window.location.hash = "";
-  //   } else {
-  //     document.body.classList.remove("no-scroll");
-  //   }
-  // }, [scrollPos]);
+  useEffect(() => {
+    if (scrollPos === 0) {
+      // document.body.classList.add("no-scroll");
+      window.location.hash = "";
+    } else {
+      // document.body.classList.remove("no-scroll");
+    }
+  }, [scrollPos]);
 
   const showNext = () => {
     window.location.hash = "#about";
